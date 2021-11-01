ab2632e65531ac78a8f7e9277c97e42126650ef1
BhargavMantha <manthabhargav@gmail.com>
2021-11-01 09:48:18 +0530

    added a componernt navigation for 1. arrows 2. skills

---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent implements OnInit {
+  todaysDate = new Date();
+  constructor() {}
+  getDate() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    return mm + '/' + dd + '/' + yyyy;
+  }
+  ngOnInit(): void {}
+}
