7be010a8e67cd45c497e0244e74783a9b1d20c7e
BhargavMantha <manthabhargav@gmail.com>
2023-09-19 19:49:00 +0530

    changed the lighting setting

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,19 +6,20 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
+  console.log(isMobile)
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={3} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={3}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={3} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
