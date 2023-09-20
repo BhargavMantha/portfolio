7815112b891a13fccdfb56e659e438fa160dabf7
BhargavMantha <manthabhargav@gmail.com>
2023-09-20 11:05:36 +0530

    changed the order

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,12 +32,12 @@ export const navLinks = [
     title: 'About',
   },
   {
-    id: 'work',
-    title: 'Work',
-  },
-  {
     id: 'experience',
     title: 'Experience',
+  },
+  {
+    id: 'work',
+    title: 'Work',
   },
 ];
 
